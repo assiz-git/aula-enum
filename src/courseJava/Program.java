2eff068946d4fb9ae34633014197b5b1b6184edf
Rogerio Assiz <rogerio.assiz@uol.com.br>
2019-01-08 21:14:10 -0200

    enum

--- src/courseJava/Program.java
+++ src/courseJava/Program.java
@@ -1,12 +1,26 @@
 package courseJava;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Bom dia!");
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 		
-
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		
+		System.out.println(os2);
+	
 	}
 
 }
